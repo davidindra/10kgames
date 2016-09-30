73b95727820628df30233a85510011d40cd22dc1
kuxa <kuxik009@gmail.com>
2016-09-30 22:38:32 +0200

    Max score 25

--- frontend/src/js/classes/blocks-object.js
+++ frontend/src/js/classes/blocks-object.js
@@ -9,7 +9,7 @@ var blocks = {
     speed: 4, // moving speed over canvas
     mySide: null, // left or right
     sp: false, // single player
-    maxScore: 50,
+    maxScore: 25,
 
     /**
      * Initialize canvas
